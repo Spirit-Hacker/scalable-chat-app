6901b9dcea7638e9b12f158706ac012eb3353484
Pranil <pranildhutraj@gmail.com>
2024-11-09 00:50:14 +0530

    initialize socket server and http server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,19 @@
 import http from "http";
+import SocketService from "./services/socket";
 
 function init() {
+  const socketService = new SocketService();
+
   const httpServer = http.createServer();
   const PORT = process.env.PORT || 8000;
+
+  socketService.io.attach(httpServer);
 
   httpServer.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
   });
+
+  socketService.initListeners();
 }
 
 init();
--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,1 +1,29 @@
-import { Server as SocketServer } from "socket.io";
+import { Server } from "socket.io";
+
+class SocketService {
+  private _io: Server;
+
+  constructor() {
+    console.log("Socket server is running...");
+    this._io = new Server();
+  }
+
+  public initListeners() {
+    const io = this.io;
+    console.log("Initialize Socket Listeners...");
+
+    io.on("connect", (socket) => {
+      console.log(`New Socket Connected ${socket.id}`);
+
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log(`New message received: ${message}`);
+      });
+    });
+  }
+
+  get io() {
+    return this._io;
+  }
+}
+
+export default SocketService;
