0db2f11789078a600cf52ad519f5d86476d19fab
Pranil <pranildhutraj@gmail.com>
2024-11-09 21:31:59 +0530

    frontend created for showing chat (not that good) and Pub/Sub architecture setup for web socket scaling

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -5,14 +5,14 @@ import Redis from "ioredis";
 
 const pub = new Redis({
   host: process.env.REDIS_AIVEN_HOST,
-  port: 22375,
-  username: process.env.REDIS_AIVEN_USERNAME,
+  port: parseInt(process.env.REDIS_AIVEN_PORT || "0", 10),
+  username: process.env.REDIS_AIVEN_USERNAME, 
   password: process.env.REDIS_AIVEN_PASSWORD,
 });
 
 const sub = new Redis({
   host: process.env.REDIS_AIVEN_HOST,
-  port: 22375,
+  port: parseInt(process.env.REDIS_AIVEN_PORT || "0", 10),
   username: process.env.REDIS_AIVEN_USERNAME,
   password: process.env.REDIS_AIVEN_PASSWORD,
 });
